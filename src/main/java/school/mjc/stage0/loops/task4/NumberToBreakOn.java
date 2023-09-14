8e2cf52b5847d8020492da6fad710d3cbe6c3fe6
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 10:07:43 +0400

    stage0module4loopsTask4

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,23 +1,17 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-
-    public static void main(String[] args) {
-        NumberToBreakOn ntb = new NumberToBreakOn();
-        ntb.printNumbersUntilWithBreakOn(5, 10); // Prints all numbers until 10, but stops when reaching 5
-    }
-
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        if (numberToGoUntil < toBreakWith) {
-            System.out.println("iterating till the end");
-            for (int i = 1; i <= numberToGoUntil; i++) {
-                System.out.println(i);
+            if (numberToGoUntil < toBreakWith) {
+                System.out.println("iterating till the end");
+                for (int i = 1; i <= numberToGoUntil; i++) {
+                    System.out.println(i);
+                }
+            } else {
+                //System.out.println( toBreakWith);
+                for (int i = 1; i <= toBreakWith; i++) {
+                    System.out.println(i);
+                }
             }
-        } else {
-            for (int i = 0; i <= numberToGoUntil; i++) {
-                if (i == toBreakWith) break;
-                System.out.println(i);
-            }
-        }
     }
 }
